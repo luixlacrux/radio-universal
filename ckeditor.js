1a92f97d57cb79ab92f32a0d4f0143767ace1554
luixlacrux <luixlacrux18@gmail.com>
2016-06-15 02:38:23 -0430

    fix routes

--- ckeditor.js
+++ ckeditor.js
@@ -1,5 +1,5 @@
 CKEDITOR.replace('content', {
-  skin: 'office2013,/assets/ckeditor/skins/office2013/'
+  skin: 'office2013,/radio-universal//assets/ckeditor/skins/office2013/'
 })
 
 CKEDITOR.config.height = 500
